bb12da1ed10a86e22723fb3cfdea40c877331aa8
James Yu <128641723+jamesdavidyu@users.noreply.github.com>
2024-07-16 00:19:11 -0400

    first draft done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -46,9 +46,8 @@ export default function Home() {
                   <Center>ANNIVERSARY!</Center>
                 </h1>
                 <h3>1 2,</h3>
-                <h3>I love you so much. I thank God that He gave me you. You are the reason I reflect on my life now and think this is what/where/who I was meant to live for.</h3>
-                <h3>To forever anniversaries, love,</h3>
-                <h3>남편</h3>
+                <h3>I love you so much. I thank God that He gave me you. You are the reason I reflect on my life now and think this is what/where/who I was meant to live for. As we continue to build our life together, I revow to never take your for granted. I also promise to mow the lawn more.</h3>
+                <h3>To forever anniversaries, love,<br />남편</h3>
               </div>
             </Box>
           </GridCol>
